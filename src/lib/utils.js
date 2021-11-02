b7262079297d96869cd0e057e2704cbfae1d3679
Satyam Mishra <satyam.mishra975@gmail.com>
2021-11-02 20:13:16 +0530

    fix env var error

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,2 @@
 import {dev} from '$app/env'
-export const api_url = dev?'http://localhost:8000':import.meta.env.BACKEND_URL
+export const api_url = dev?'http://localhost:8000':import.meta.env.VITE_BACKEND_URL
